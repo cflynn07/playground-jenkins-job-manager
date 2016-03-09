39d419e542d8220fb85c1c18430c35fb703aaf73
Casey Flynn <cflynn.us@gmail.com>
2016-03-09 12:26:25 -0800

    Initial commit

---
+++ jobs/tutorial.groovy
@@ -0,0 +1,12 @@
+job('DSL-Tutorial-1-Test') {
+  scm {
+    git('git://github.com/cflynn07/simple-producer-consumer.git')
+  }
+  triggers {
+    scm('*/15 * * * *')
+  }
+  steps {
+    # maven('-e clean test')
+
+  }
+}
